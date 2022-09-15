a5130b1ffdf835eda59483d36b9d36f2fb637e47
Beatriz <beatrizramalho.esteves@gmail.com>
2022-09-15 18:35:04 -0300

    todo com componentes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="" id="app">
+    <div class="container grid-xs py-2">
+      <img class="img-responsive img-logo" src="@/assets/logo.png" alt="Logo" />
+      <form @submit.prevent="addTodo(todoComponent)">
+        <div class="input-group">
+          <input
+            type="text"
+            v-model="todoComponent.description"
+            class="form-input"
+            placeholder="Novo todo"
+          />
+          <button class="btn btn-primary input-group-btn">Adicionar</button>
+        </div>
+      </form>
+      <div class="todo-list">
+        <todoComponent
+          v-for="t in todos"
+          :key="t.id"
+          @toggle="toggleTodo"
+          @remove="removeTodo"
+          :todoComponent="t"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoComponent from "./components/TodoComponent";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "app",
+  components: { TodoComponent },
+  data() {
+    return { todos: [], todoComponent: { checked: false } };
+  },
+  methods: {
+    addTodo(todoComponent) {
+      todoComponent.id = Date.now();
+      this.todos.push(todoComponent);
+      this.todoComponent = { checked: false };
+    },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    toggleTodo(todoComponent) {
+      const index = this.todos.findIndex(
+        (item) => item.id === todoComponent.id
+      );
+      if (index > -1) {
+        const checked = !this.todos[index].checked;
+        this.$set(this.todos, index, { ...this.todos[index], checked });
+      }
+    },
+    removeTodo(todoComponent) {
+      const index = this.todos.findIndex(
+        (item) => item.id === todoComponent.id
+      );
+      if (index > -1) {
+        this.$delete(this.todos, index);
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+.img-logo {
+  max-width: 200px;
+  margin: 0 auto;
+}
+
+.todo-list {
+  padding-top: 2rem;
 }
 </style>
---
+++ src/components/TodoComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="tile" :class="{ checked: todoComponent.checked }">
+    <div class="tile-icon">
+      <i
+        class="icon"
+        :class="todoComponent.checked ? 'icon-check' : 'icon-time'"
+      ></i>
+    </div>
+    <div class="tile-content">
+      <div class="tile-subtitle">{{ todoComponent.description }}</div>
+      <div>{{ todoComponent.checked }}</div>
+    </div>
+    <div class="tile-action">
+      <button @click="$emit('toggle', todoComponent)" class="btn btn-link">
+        Concluído
+      </button>
+      <button @click="$emit('remove', todoComponent)" class="btn btn-link">
+        <span class="text-error">Remover</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todoComponent: { type: Object, required: true },
+  },
+};
+</script>
+
+<style scoped>
+.checked {
+  text-decoration: line-through;
+  color: lightgray;
+}
+</style>
